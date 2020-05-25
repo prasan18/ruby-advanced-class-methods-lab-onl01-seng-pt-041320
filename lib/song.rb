cafa7729dcd754910f69442000c7d0be98f1ed7f
ANTONY JUDE PRASAN POOBALARAYAN <prasan29@yahoo.com>
2020-05-25 15:04:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,7 +12,6 @@ class Song
 
   def self.create
     song = self.new
-    #@@all << song
     song.save
     song
   end
@@ -35,17 +34,12 @@ class Song
   end
   
   def self.find_or_create_by_name(name)
-      #if self.find_by_name(name) == nil 
-        #self.create_by_name(name)
-      #else
-        #self.find_by_name(name)
-      #end
+      
       self.find_by_name(name) || self.create_by_name(name)
-      #the above statement is saying do this(if it is true) or that (if the first thing is not true and the second thing is true)
   end
   
   def self.alphabetical()
-    #returns all the songs instances in ascending (a-z) alphabetical order.
+
     @@all.sort_by{|x| x.name}
   end
   
@@ -58,7 +52,7 @@ class Song
   end
   
   def self.create_from_filename(name)
-  #class method should not only parse the filename correctly but should also save the song
+
     song = self.new
     song.name = (name.split(" - ")[1].chomp(".mp3"))
     song.artist_name = (name.split(" - ")[0])
@@ -67,39 +61,11 @@ class Song
   end
   
   def self.destroy_all()
-    #reset the state of the @@all class variable to an empty array thereby deleting all previous song instances.
+    
     @@all.clear
   end
 
 end
 
-
-
-#this is for the create_from_filename method & destroy_all
-#song = Song.create_from_filename("Taylor Swift - Blank Space.mp3")
-#song = Song.create_from_filename("Test 02 - Test_02.mp3")
-#song = Song.create_from_filename("Test 03 - Test_03.mp3")
-#puts Song.all.inspect
-#Song.destroy_all
-#puts Song.all.inspect
-
-#song.name #=> "Blank Space"
-#song.artist_name #=> "Taylor Swift"
-
-
-#this is for new_from_filename method
-#song = Song.new_from_filename("Taylor Swift - Blank Space.mp3")
-#song.name #=> "Blank Space"
-#song.artist_name #=> "Taylor Swift"
-
-
-#this is for the alphabetical method
-#song = Song.create_by_name("Cranberry")
-#song = Song.create_by_name("Apples")
-#song = Song.create_by_name("Berry")
-#Song.alphabetical
-
-
-#this is for the find_or_create_by_name method
 song_1 = Song.find_or_create_by_name("Blank Space")
 song_2 = Song.find_or_create_by_name("Blank Space")
